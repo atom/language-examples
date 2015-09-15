b4b4e88d28d9c5c437749684222c3858304a7440
simurai <simulus@gmail.com>
2015-09-15 16:01:26 +0900

    Update PHP

--- languages/php.php
+++ languages/php.php
@@ -1,5 +1,17 @@
-<?php
-  public function name($something, $other) {
 
+<!-- 1. Example -->
+
+<?php
+  foreach ($_SERVER as $key=>$value)
+  {
+    echo $key."=".$value;
+    echo "<br><br>";
   }
+?>
+
+
+<!-- 2. Tests -->
+
+<?php
+  public function name($something, $other) {}
 ?>
