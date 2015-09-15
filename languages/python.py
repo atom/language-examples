cfe6d5abec74b445232e3c0f14a3ecdaa84b15fd
simurai <simulus@gmail.com>
2015-09-15 15:48:21 +0900

    Update Python

--- languages/python.py
+++ languages/python.py
@@ -1,3 +1,23 @@
+
+# 1. Example ------------------------
+
+import unittest
+def median(pool):
+    copy = sorted(pool)
+    size = len(copy)
+    if size % 2 == 1:
+        return copy[(size - 1) / 2]
+    else:
+        return (copy[size/2 - 1] + copy[size/2]) / 2
+class TestMedian(unittest.TestCase):
+    def testMedian(self):
+        self.failUnlessEqual(median([2, 9, 9, 7, 9, 2, 4, 5, 8]), 7)
+if __name__ == '__main__':
+    unittest.main()
+
+
+# 2. Tests ------------------------
+
 @requires_authorization
 def somefunc(param1='', param2=0):
     r'''A docstring'''
